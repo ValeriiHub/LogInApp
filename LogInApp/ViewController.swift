fc196cd459e50ae7078049ee0b46eec9e16b49c7
ValeriyHub <p04taera@gmail.com>
2021-04-30 00:35:37 +0300

    Add UI elements and Alerts

--- LogInApp/ViewController.swift
+++ LogInApp/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    //MARK: IB Actions
+    
+    @IBAction func logInAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotUserNameAction() {
+        showAlert(title: "Oops!", message: "Your name is User 😉")
+    }
+    
+    @IBAction func forgotPasswordAction() {
+        showAlert(title: "Oops!", message: "Your password is Password ☺️")
+    }
+    
 }
 
+extension ViewController {
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+       
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okAction)
+              
+        present(alertController, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+}
